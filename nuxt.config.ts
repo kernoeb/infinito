9cd835b6add952ae2c94160c337b05e42fcb5314
kernoeb <kernoeb@protonmail.com>
2023-10-07 00:05:23 +0200

    chore: add icons and banner

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,23 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   app: {
     head: {
       title: 'Infinito',
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1'
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        { name: 'description', content: 'Free and open-source web app to find and share stuff' },
+        { name: 'author', content: 'kernoeb' },
+        { name: 'keywords', content: 'infinito, free, open-source, web, app, find, share, stuff' },
+        { name: 'language', content: 'French' },
+        { name: 'robots', content: 'index,follow' },
+
+        // Twitter
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:url', content: 'https://infinito.noewen.com' },
+        { name: 'twitter:title', content: 'Infinito' },
+        { name: 'twitter:description', content: 'Free and open-source web app to find and share stuff' },
+        { name: 'twitter:image', content: 'https://infinito.noewen.com/banner.png' }
+      ]
     }
   },
   modules: [
