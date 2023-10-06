1d3df094002c5d73d3ad3ddae138759de3257d8a
kernoeb <kernoeb@protonmail.com>
2023-10-07 00:08:52 +0200

    chore: meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,37 @@
+const TITLE = 'Infinito'
+const DOMAIN = 'infinito.noewen.com'
+const URL = 'https://' + DOMAIN
+const DESCRIPTION = 'Free and open-source web app to find and share stuff'
+const BANNER = `${URL}/banner.png`
+const KEYWORDS = ['infinito', 'free', 'open-source', 'web', 'app', 'find', 'share', 'stuff']
+const AUTHOR = 'kernoeb'
+
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'Infinito',
+      title: TITLE,
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       meta: [
-        { name: 'description', content: 'Free and open-source web app to find and share stuff' },
-        { name: 'author', content: 'kernoeb' },
-        { name: 'keywords', content: 'infinito, free, open-source, web, app, find, share, stuff' },
+        { name: 'description', content: DESCRIPTION },
+        { name: 'author', content: AUTHOR },
+        { name: 'keywords', content: KEYWORDS.join(', ') },
         { name: 'language', content: 'French' },
         { name: 'robots', content: 'index,follow' },
 
+        // Facebook
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: URL + '/' },
+        { hid: 'og:title', property: 'og:title', content: TITLE },
+        { hid: 'og:description', property: 'og:description', content: DESCRIPTION },
+        { property: 'og:image', content: BANNER },
+
         // Twitter
         { name: 'twitter:card', content: 'summary_large_image' },
-        { name: 'twitter:url', content: 'https://infinito.noewen.com' },
-        { name: 'twitter:title', content: 'Infinito' },
-        { name: 'twitter:description', content: 'Free and open-source web app to find and share stuff' },
-        { name: 'twitter:image', content: 'https://infinito.noewen.com/banner.png' }
+        { name: 'twitter:url', content: URL + '/' },
+        { name: 'twitter:title', content: TITLE },
+        { name: 'twitter:description', content: DESCRIPTION },
+        { name: 'twitter:image', content: BANNER }
       ]
     }
   },
